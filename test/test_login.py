bafd9f64dffe293affeb7c528599ccd408e58d6b
Aleksey Verzhinskiy-Ryng <a.verzhinskiy-ryng@melsoft-games.com>
2020-09-13 02:20:53 +0300

    Реализованы проверки списков проектов через SOAP

--- test/test_login.py
+++ test/test_login.py
@@ -1,8 +1,4 @@
 def test_login(app):
     app.session.login("administrator", "root")
-    # assert app.session.is_logged_in_as("administrator")
+    assert app.session.is_logged_in_as("administrator")
     assert app.soap.can_login("administrator", "root")
-    prjct_lists = []
-    get_prjct =  app.soap.get_project_list_user("administrator", "root")
-    prjct_lists.append(get_prjct)
-    print("\nnew_project: ", prjct_lists)
