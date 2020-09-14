ee07710d2fb5578bfe2d1906344ee7366c5878d4
Aleksey Verzhinskiy-Ryng <a.verzhinskiy-ryng@melsoft-games.com>
2020-09-14 14:02:48 +0300

    Поправил адрес 'ендпоинта' асть берется из таргет.джейсон

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,7 +8,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +17,7 @@ class SoapHelper:
 
     def get_project_list_user(self, username, password):
         self.can_login(username, password)
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "api/soap/mantisconnect.php?wsdl")
 
         try:
             response = client.service.mc_projects_get_user_accessible(username, password)
